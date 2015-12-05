d5ec4eb8cdb4a7883890c00ee79aa9591ea86442
Morgan Lieberthal <morgan@jmorgan.org>
2015-12-05 16:29:50 -0700

    Initial Commit

---
+++ CoreExtensions/CoreExtensions.h
@@ -0,0 +1,19 @@
+//
+//  CoreExtensions.h
+//  CoreExtensions
+//
+//  Created by Morgan Lieberthal on 12/5/15.
+//  Copyright © 2015 Morgan Lieberthal. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for CoreExtensions.
+FOUNDATION_EXPORT double CoreExtensionsVersionNumber;
+
+//! Project version string for CoreExtensions.
+FOUNDATION_EXPORT const unsigned char CoreExtensionsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoreExtensions/PublicHeader.h>
+
+
